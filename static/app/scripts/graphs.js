8f776eaa45f4c4d72caaab9a7bb490ff58a3e024
stelaseldano <stella.plamenova@gmail.com>
2015-12-11 10:51:33 +0100

    inputs

--- static/app/scripts/graphs.js
+++ static/app/scripts/graphs.js
@@ -98,15 +98,16 @@ app.graphs = (function() {
 		
 		/* handle styles */
 		self.edgesLayer.style = {
-			strokeColor: 'yellowgreen'
+			strokeColor: '#48F3AF'
 		};
 		self.nodesLayer.style = {
-			fillColor: 'skyblue'
+			fillColor: '#1D1E21'
 		};
 		self.textLayer.style = {
-			fillColor: 'black',
-			fontFamily: 'Fira Sans',
-			fontSize: 9
+			fillColor: 'white',
+			fontFamily: 'monospace',
+			fontWeight: 'bold',
+			fontSize: 12
 		};
 		
 		/* re-draw the canvas */
@@ -259,7 +260,7 @@ app.graphs = (function() {
 		
 		self.circleItem = new paper.Path.Circle({
 			parent: self.graph.nodesLayer,
-			radius: 12
+			radius: 15
 		});
 		
 		self.textItem = new paper.PointText({
