e12a49d2884ed6bdb2cb5d6b27035a43b290976b
pavelsof <pavelsof@gmail.com>
2018-04-04 12:57:42 +0200

    Updated the pip dependencies.

--- app/views/landing.py
+++ app/views/landing.py
@@ -1,6 +1,4 @@
-from django.conf import settings
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.base import View
@@ -10,29 +8,23 @@ from app.models import Globe
 
 
 class LandingView(View):
-	
+
 	@method_decorator(ensure_csrf_cookie)
 	def dispatch(self, *args, **kwargs):
 		"""
 		Ensures sending of csrf cookie.
 		"""
 		return super().dispatch(*args, **kwargs)
-	
-	
+
+
 	def get(self, request):
 		"""
 		Renders the landing page.
 		"""
 		globes = Globe.objects.all()
-		
-		return render_to_response(
-			'landing.html',
-			{
-				'globes': globes,
-				'EARTH': globes[0].geo_json
-			},
-			context_instance = RequestContext(request)
-		)
 
+		context = {
+			'globes': globes,
+			'EARTH': globes[0].geo_json }
 
-
+		return render(request, 'landing.html', context)
